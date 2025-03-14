f74e94a43930b72277261bdcf1aaaca26aeab475
dwight taylor <dtaylo37@calpoly.edu>
2025-03-13 19:41:03 -0700

    Completing TE6 business

--- packages/backend/exerciseRoutes.js
+++ packages/backend/exerciseRoutes.js
@@ -1,5 +1,5 @@
-import express from "express"
-import { getExercises } from "./exerciseController.js"
+import express from "express";
+import { getExercises } from "./exerciseController.js";
 
 const router = express.Router();
 
